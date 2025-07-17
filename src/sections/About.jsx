c0c6076954693bcd2be12b6090a60b47aa077864
Moh-svg04 <mohamed.gueye@etu.univ-lehavre.fr>
2025-07-17 18:15:04 +0200

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -17,10 +17,9 @@ const About = () => {
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
           />
           <div className="z-10">
-            <p className="headtext">Hi, I'm Ali Sanati</p>
+            <p className="headtext">Hi, I'm  Mohamed gueye aka moe </p>
             <p className="subtext">
-              Over the last 4 years, I developed my frontend and backend dev
-              skills to deliver dynamic and software and web applications.
+             Actuellement étudiant en L3 informatique, je me forme activement à la data science. Grâce à mes bases en Python, R, SQL, algorithmique et statistiques, je développe des compétences solides pour analyser les données, modéliser des problèmes concrets et me spécialiser dans l’intelligence artificielle.
             </p>
           </div>
           <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
