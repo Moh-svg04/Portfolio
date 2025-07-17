256c2f51210897edd645ecd000efdb6a3bf9bc00
Moh-svg04 <mohamed.gueye@etu.univ-lehavre.fr>
2025-07-17 17:49:17 +0200

    Update HeroText.jsx

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -2,7 +2,7 @@ import { FlipWords } from "./FlipWords";
 import { motion } from "motion/react";
 
 const HeroText = () => {
-  const words = ["Secure", "Modern", "Scalable"];
+  const words = ["Insightful", "Innovative", "Predictive"];
   const variants = {
     hidden: { opacity: 0, x: -50 },
     visible: { opacity: 1, x: 0 },
@@ -18,7 +18,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi I'm Ali
+          Hi I'm Mohamed aka Moe 
         </motion.h1>
         <div className="flex flex-col items-start">
           <motion.p
@@ -28,7 +28,7 @@ const HeroText = () => {
             animate="visible"
             transition={{ delay: 1.2 }}
           >
-            A Developer <br /> Dedicated to Crafting
+            An aspiring Data Scientist <br /> AI and technology enthusiast, and lifelong learner
           </motion.p>
           <motion.div
             variants={variants}
@@ -48,7 +48,7 @@ const HeroText = () => {
             animate="visible"
             transition={{ delay: 1.8 }}
           >
-            Web Solutions
+            Data-Driven Solutions
           </motion.p>
         </div>
       </div>
